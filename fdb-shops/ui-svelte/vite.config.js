451541f6fba0036d3261ec70db53308566b598df
Farrofa DeBacon <FarrofaDeBacon@users.noreply.github.com>
2026-09-21 21:12:01 -0300

    test(ui): remove plugin-legacy to isolate CEF effect_orphan cause

--- fdb-shops/ui-svelte/vite.config.js
+++ fdb-shops/ui-svelte/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
-    svelte(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
+    svelte()
   ],
   base: './',
   build: {
